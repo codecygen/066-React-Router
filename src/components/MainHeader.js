4397a1aa9c39da3f9dd86c1b0b05dfa862f26f52
gingercook <vahitaras@gmail.com>
2022-04-13 20:45:47 -0400

    NavLink component added to show active classes

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,22 +1,32 @@
 import React from 'react';
 
+import classes from './MainHeader.module.css';
+
 // React-Router-DOM-Link-Click-Prevent-HTTP-Request-To-Server
 // Loads pages without sending HTTP request to server
 // Fake navigation method which prevents server to be overloaded
-import { Link } from 'react-router-dom';
+// Link component works just like NavLink but it does not highlight the active link
+// ========================================
+// import { Link } from 'react-router-dom';
+// ========================================
+import { NavLink } from 'react-router-dom';
 
 const MainHeader = () => {
     return (
-        <header>
+        <header className={classes.header}>
             <ul>
                 <li>
                     {/* React-Router-DOM-Link-Click-Prevent-HTTP-Request-To-Server */}
-                    <Link to="/welcome">Welcome</Link>
+                    <NavLink activeClassName={classes.active} to="/welcome">
+                        Welcome
+                    </NavLink>
                 </li>
 
                 <li>
                     {/* React-Router-DOM-Link-Click-Prevent-HTTP-Request-To-Server */}
-                    <Link to="/products">Products</Link>
+                    <NavLink activeClassName={classes.active} to="/products">
+                        Products
+                    </NavLink>
                 </li>
             </ul>
         </header>
