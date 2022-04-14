947fea550a6a7447fcc658a86b1845a453ca8906
gingercook <vahitaras@gmail.com>
2022-04-13 20:21:58 -0400

    basic react router setup completed

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ This section is about building a multi-page SPA by using React Router.
 
 NPM Package for React Router:
 - npm i react-router-dom
+
+Keywords:
+- React-Router-DOM
--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-function App() {
+// React-Router-DOM
+import { Route } from "react-router-dom";
+
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+
+const App = () => {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      {/* React-Router-DOM */}
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+
+      {/* React-Router-DOM */}
+      <Route path="/products">
+        <Products />
+      </Route>
     </div>
   );
-}
+};
 
 export default App;
